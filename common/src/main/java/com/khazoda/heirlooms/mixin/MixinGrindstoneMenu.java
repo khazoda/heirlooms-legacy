1439cd4f565e704fbf8914cc37009d7610802021
June|Khazoda <khazoda@gmail.com>
2025-10-05 02:46:55 +0100

    incorporate commits bdcae3d, cf44a93 from 1.21.9 branch

---
+++ common/src/main/java/com/khazoda/heirlooms/mixin/MixinGrindstoneMenu.java
@@ -0,0 +1,22 @@
+package com.khazoda.heirlooms.mixin;
+
+import com.khazoda.heirlooms.HeirloomsMod;
+import net.minecraft.world.inventory.GrindstoneMenu;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/* FOR GRINDSTONE */
+/* Targets the method that's called to disenchant an item */
+@Mixin(GrindstoneMenu.class)
+public class MixinGrindstoneMenu {
+  @Inject(method = "removeNonCursesFrom", at = @At(value = "RETURN"))
+  private void heirlooms$onEnchantmentRemoved(ItemStack item, CallbackInfoReturnable<ItemStack> cir) {
+    if (item.has(HeirloomsMod.ENCHANTED_TIMESTAMP) || item.has(HeirloomsMod.ENCHANTED_BY)) {
+      item.remove(HeirloomsMod.ENCHANTED_TIMESTAMP);
+      item.remove(HeirloomsMod.ENCHANTED_BY);
+    }
+  }
+}
--- fabric/src/main/java/com/khazoda/heirlooms/TooltipHandler.java
+++ fabric/src/main/java/com/khazoda/heirlooms/TooltipHandler.java
@@ -1,20 +1,14 @@
 package com.khazoda.heirlooms;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.world.item.TooltipFlag;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.TextStyle;
 import java.util.List;
-import java.util.Locale;
 
 public class TooltipHandler implements ItemTooltipCallback {
 
@@ -24,76 +18,9 @@ public class TooltipHandler implements ItemTooltipCallback {
 
   @Override
   public void getTooltip(ItemStack stack, Item.TooltipContext tooltipContext, TooltipFlag tooltipFlag, List<Component> list) {
-    String crafted_timestamp = stack.get(HeirloomsMod.CRAFTED_TIMESTAMP);
-    String crafted_by = stack.get(HeirloomsMod.CRAFTED_BY);
-    int craftedTextColour = 9068607;
+    boolean pressedCTRL = InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), InputConstants.KEY_LCONTROL);
+    boolean pressedALT = InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), InputConstants.KEY_LALT);
 
-    String enchanted_timestamp = stack.get(HeirloomsMod.ENCHANTED_TIMESTAMP);
-    String enchanted_by = stack.get(HeirloomsMod.ENCHANTED_BY);
-    int enchantedTextColour = 4157834;
-
-    /* Only apply to items with one of my data components */
-    if (crafted_timestamp == null && enchanted_timestamp == null) return;
-
-    /* Forge correct button prompts based on player's current pressed key(s) */
-    MutableComponent buttonPrompt = Component.empty();
-    boolean pressedCTRL = Screen.hasControlDown();
-    boolean pressedALT = Screen.hasAltDown();
-    boolean existsCraftData = crafted_timestamp != null;
-    boolean existsEnchantData = enchanted_timestamp != null;
-    if (existsCraftData && !pressedCTRL) buttonPrompt.append(Component.literal("[ᴄᴛʀʟ]").withColor(craftedTextColour));
-    if (existsCraftData && !pressedCTRL && existsEnchantData && !pressedALT) buttonPrompt.append(Component.literal(" "));
-    if (existsEnchantData && !pressedALT) buttonPrompt.append(Component.literal("[ᴀʟᴛ]").withColor(enchantedTextColour));
-    if (!buttonPrompt.getString().isEmpty()) list.add(buttonPrompt);
-
-    /* Add crafting tooltip */
-    if (crafted_timestamp != null) {
-      if (crafted_by != null) {
-        try {
-          if (Screen.hasControlDown()) {
-            Instant instant = Instant.parse(crafted_timestamp);
-            ZonedDateTime zonedDateTime = instant.atZone(ZoneId.systemDefault());
-
-            int year = zonedDateTime.getYear();
-            int month = zonedDateTime.getMonthValue();
-            int day = zonedDateTime.getDayOfMonth();
-
-            String languageCode = Minecraft.getInstance().getLanguageManager().getSelected();
-            Locale locale = Locale.forLanguageTag(languageCode.replace("_", "-"));
-            String monthName = zonedDateTime.getMonth().getDisplayName(TextStyle.FULL, locale);
-
-            list.add(Component.translatable("tooltip.heirlooms.crafted_by", crafted_by).withColor(craftedTextColour));
-            list.add(Component.translatable("tooltip.heirlooms.date", day, month, monthName, year).withColor(craftedTextColour));
-          }
-        } catch (Exception ignored) {
-          System.out.println("Heirlooms mod error [1] - please report on the issue tracker");
-        }
-      }
-    }
-
-    /* Add enchantment tooltip */
-    if (enchanted_timestamp != null) {
-      if (enchanted_by != null) {
-        try {
-          if (Screen.hasAltDown()) {
-            Instant instant = Instant.parse(enchanted_timestamp);
-            ZonedDateTime zonedDateTime = instant.atZone(ZoneId.systemDefault());
-
-            int year = zonedDateTime.getYear();
-            int month = zonedDateTime.getMonthValue();
-            int day = zonedDateTime.getDayOfMonth();
-
-            String languageCode = Minecraft.getInstance().getLanguageManager().getSelected();
-            Locale locale = Locale.forLanguageTag(languageCode.replace("_", "-"));
-            String monthName = zonedDateTime.getMonth().getDisplayName(TextStyle.FULL, locale);
-
-            list.add(Component.translatable("tooltip.heirlooms.enchanted_by", enchanted_by).withColor(enchantedTextColour));
-            list.add(Component.translatable("tooltip.heirlooms.date", day, month, monthName, year).withColor(enchantedTextColour));
-          }
-        } catch (Exception ignored) {
-          System.out.println("Heirlooms mod error [1] - please report on the issue tracker");
-        }
-      }
-    }
+    CommonTooltipHandler.handleTooltip(stack, list, tooltipFlag, pressedCTRL, pressedALT);
   }
 }
